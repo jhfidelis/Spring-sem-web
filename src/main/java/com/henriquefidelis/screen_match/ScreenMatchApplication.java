aed6e6347ab7cd09d9e319295e516476a783f0b1
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-06-09 21:23:30 -0300

    Buscadndo dados sobre um episódio

--- src/main/java/com/henriquefidelis/screen_match/ScreenMatchApplication.java
+++ src/main/java/com/henriquefidelis/screen_match/ScreenMatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.henriquefidelis.screen_match.models.DadosEpisodio;
 import com.henriquefidelis.screen_match.models.DadosSerie;
 import com.henriquefidelis.screen_match.service.ConsumoAPI;
 import com.henriquefidelis.screen_match.service.ConverteDados;
@@ -24,6 +25,10 @@ public class ScreenMatchApplication implements CommandLineRunner {
 		ConverteDados conversao = new ConverteDados();
 		DadosSerie dados = conversao.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=everybody+hates+chris&season=3&episode=2&apikey=5282043b");
+		DadosEpisodio dadosEpisodio = conversao.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 
 }
