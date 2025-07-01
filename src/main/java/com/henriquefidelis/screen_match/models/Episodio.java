530d7330d490f972615d39f94701ae3a877ec691
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2025-07-01 20:14:18 -0300

    Relacionando tabela de séries com tabela de episódios

--- src/main/java/com/henriquefidelis/screen_match/models/Episodio.java
+++ src/main/java/com/henriquefidelis/screen_match/models/Episodio.java
@@ -3,13 +3,29 @@ package com.henriquefidelis.screen_match.models;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_episodios")
 public class Episodio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataDeLancamento;
+    
+    @ManyToOne
+    private Serie serie;
     
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -29,10 +45,18 @@ public class Episodio {
         }
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Integer getTemporada() {
         return temporada;
     }
-    
+
     public void setTemporada(Integer temporada) {
         this.temporada = temporada;
     }
@@ -40,32 +64,41 @@ public class Episodio {
     public String getTitulo() {
         return titulo;
     }
-    
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
     public Integer getNumeroEpisodio() {
         return numeroEpisodio;
     }
-    
+
     public void setNumeroEpisodio(Integer numero) {
         this.numeroEpisodio = numero;
     }
-    
+
     public Double getAvaliacao() {
         return avaliacao;
     }
-    
+
     public void setAvaliacao(Double avaliacao) {
         this.avaliacao = avaliacao;
     }
-    
+
     public LocalDate getDataDeLancamento() {
         return dataDeLancamento;
     }
-    
+
     public void setDataDeLancamento(LocalDate dataDeLancamento) {
         this.dataDeLancamento = dataDeLancamento;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
     
     @Override
--- src/main/java/com/henriquefidelis/screen_match/models/Serie.java
+++ src/main/java/com/henriquefidelis/screen_match/models/Serie.java
@@ -11,8 +11,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_series")
@@ -35,7 +35,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {
