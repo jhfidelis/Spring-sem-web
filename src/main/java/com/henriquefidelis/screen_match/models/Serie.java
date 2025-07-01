7603311d35acbe79119474e525a2507277584302
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2025-06-30 22:17:26 -0300

    Listando as séries do banco de dados

--- src/main/java/com/henriquefidelis/screen_match/models/Serie.java
+++ src/main/java/com/henriquefidelis/screen_match/models/Serie.java
@@ -37,6 +37,9 @@ public class Serie {
 
     @Transient
     private List<Episodio> episodios = new ArrayList<>();
+
+    public Serie() {
+    }
     
     public Serie(DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
