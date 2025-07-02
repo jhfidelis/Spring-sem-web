bff4791bb12812baed21c1987814c9e3c26f2489
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2025-07-02 20:23:58 -0300

    Ajustando a chave estrangeira

--- src/main/java/com/henriquefidelis/screen_match/models/Serie.java
+++ src/main/java/com/henriquefidelis/screen_match/models/Serie.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -36,7 +37,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {
@@ -121,13 +122,14 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
     @Override
     public String toString() {
         return "[genero=" + genero + ", titulo=" + titulo + ", totalDeTemporadas=" + totalDeTemporadas + ", avaliacao=" + avaliacao
-                + ", atores=" + atores + ", poster=" + poster + ", sinopse=" + sinopse + "]";
+                + ", atores=" + atores + ", poster=" + poster + ", sinopse=" + sinopse + ", episodios=" + episodios + "]";
     }
 
     
