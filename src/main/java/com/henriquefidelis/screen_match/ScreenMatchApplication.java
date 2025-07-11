9128cf31f0c02a520bac1556748620b698d9ee9a
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2025-07-10 22:17:37 -0300

    transformando a aplicação em API

--- src/main/java/com/henriquefidelis/screen_match/ScreenMatchApplication.java
+++ src/main/java/com/henriquefidelis/screen_match/ScreenMatchApplication.java
@@ -1,27 +1,13 @@
 package com.henriquefidelis.screen_match;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.henriquefidelis.screen_match.main.Principal;
-import com.henriquefidelis.screen_match.repository.SerieRepository;
-
 @SpringBootApplication
-public class ScreenMatchApplication implements CommandLineRunner {
-
-	@Autowired
-    private SerieRepository repository;
+public class ScreenMatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repository);
-		principal.exibirMenu();
 	}
 
 }
