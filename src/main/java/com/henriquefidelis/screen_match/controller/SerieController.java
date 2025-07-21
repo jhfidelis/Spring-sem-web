e5d6bd50958d07ef15d7be18c2127c97caf3e378
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-07-21 17:38:05 -0300

    Exibindo top 5 séries do banco de dados

--- src/main/java/com/henriquefidelis/screen_match/controller/SerieController.java
+++ src/main/java/com/henriquefidelis/screen_match/controller/SerieController.java
@@ -3,20 +3,27 @@ package com.henriquefidelis.screen_match.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.henriquefidelis.screen_match.dto.SerieDTO;
 import com.henriquefidelis.screen_match.service.SerieService;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries() {
         return service.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return service.obterTop5Series();
     }
 
 }
--- src/main/java/com/henriquefidelis/screen_match/service/SerieService.java
+++ src/main/java/com/henriquefidelis/screen_match/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henriquefidelis.screen_match.dto.SerieDTO;
+import com.henriquefidelis.screen_match.models.Serie;
 import com.henriquefidelis.screen_match.repository.SerieRepository;
 
 @Service
@@ -16,7 +17,15 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repository.findAll().stream()
+        return converterDados(repository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converterDados(repository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converterDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalDeTemporadas(),
                         s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
